e4288c117922c03e73c70efa7c8db1a047807af1
Kriss Gardner <krissrobert12@gmail.com>
2024-04-22 17:43:35 +0300

    Fix layouts

--- src/App.js
+++ src/App.js
@@ -4,18 +4,33 @@ import LoginForm from './Components/LoginForm/LoginForm';
 import Register from './Components/Register/Register';
 import Forgot from './Components/Forgot/Forgot';
 import Homepage from './Homepage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 function App() {
+
+  // we need to check for auth
+  // we figure out if the client has a token
+
+  const token = null;
+
+  if (token) {
+    return <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot" element={<Forgot />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  }
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot" element={<Forgot />} />
-          <Route path="/home" element={<Homepage />} />
+          <Route path="/" element={<Homepage />} />
+          {/*  HERE you can add profiles or other stuff */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </div>
